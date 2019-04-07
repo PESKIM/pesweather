10d60fa2e821ba6d0ddd1e34efe26c73faa673ec
PES <21ckpg@gmail.com>
2019-04-07 15:13:18 +0900

    1.0.0

--- pesweather/App.js
+++ pesweather/App.js
@@ -10,12 +10,18 @@ export default class App extends Component {
     error: null,
     temprerature: null,
     name: null,
+    location: null,
+    aqi: null,
   };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position =>{
-        this._getWeather(position.coords.latitude, position.coords.longitude)
+        this.setState(
+          console.log(position)
+        )
+        this._getWeather_1(position.coords.latitude, position.coords.longitude)
+        this._getWeather_2(position.coords.latitude, position.coords.longitude)
         },
     
       error => {
@@ -25,24 +31,36 @@ export default class App extends Component {
       }
     );
   }
-  _getWeather= (lat, lon) => {
+  _getWeather_1= (lat, lon) => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
     .then(response => response.json())
     .then(json=>{
       this.setState({
-        temprerature:json.main.temp,
+        temperature:json.main.temp,
         name:json.weather[0].main,
-        isLoaded:true
+        location:json.name,
+        isLoaded:true,
+
+      })
+    });
+  };
+
+  _getWeather_2= (lat, lon) => {
+    fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=3bd95e01d2fb8182f090ba66fc69eab01a3595c4`)
+    .then(response => response.json())
+    .then(json=>{
+      this.setState({
+        aqi:json.data.aqi
       })
     });
   };
   
 
   render() {
-    const { isLoaded, error } = this.state;
+    const { isLoaded, error, temperature, name, location, aqi } = this.state;
     return (
       <View style={styles.container}>
-        {isLoaded ? (<Weather />) : (
+        {isLoaded ? (<Weather temp={Math.floor(temperature - 273.15)} weatherName={name} location={location} aqi={aqi} />) : (
         <View style={styles.loading}>
           <Text style={styles.loadingText}>Welcome</Text>
           {error ? <Text style={styles.errorText}>{error}</Text> : null}
--- pesweather/Weather.js
+++ pesweather/Weather.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View,} from 'react-native'
 import {LinearGradient} from "expo";
 import { Ionicons } from '@expo/vector-icons';
-
+import PropTypes from "prop-types";
+/*
 export default class Weather extends Component {
     render(){
         return(
@@ -21,6 +22,88 @@ export default class Weather extends Component {
         );
     }
 }
+*/
+
+const weatherCases = {
+    Rain: {
+        colors: ["#00C6FB", "#005BEA"],
+        title: "Raining like a MF",
+        subtitle: "For more info look outside",
+        icon: "ios-rainy"
+      },
+      Clear: {
+        colors: ["#FEF253", "#FF7300"],
+        title: "해해해해해",
+        subtitle: "Go get your ass burnt",
+        icon: "ios-sunny"
+      },
+      Thunderstorm: {
+        colors: ["#00ECBC", "#007ADF"],
+        title: "Thunderstorm in the house",
+        subtitle: "Actually, outside of the house",
+        icon: "ios-lightning"
+      },
+      Clouds: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "나가지마라",
+        icon: "ios-cloudy"
+      },
+      Snow: {
+        colors: ["#7DE2FC", "#B9B6E5"],
+        title: "Cold as balls",
+        subtitle: "Do you want to build a snowman? Fuck no.",
+        icon: "ios-snowy"
+      },
+      Drizzle: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "Is like rain, but gay 🏳️‍🌈",
+        icon: "ios-hail"
+      },
+      Haze: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Haze",
+        subtitle: "Don't know what that is 💩",
+        icon: "ios-hail"
+      },
+      Mist: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Mist!",
+        subtitle: "It's like you have no glasses on.",
+        icon: "ios-fog"
+      }
+};
+
+function Weather({temp, weatherName, location, aqi}) {
+    console.log(weatherName)
+    return(
+        <LinearGradient
+            colors={weatherCases[weatherName].colors}
+            style={styles.container}>
+        <View style={styles.upper}>
+            <Ionicons color="white" size={144} name={weatherCases[weatherName].icon} />
+            <Text style={styles.temp}>{temp}^</Text>
+        </View>
+        <View style={styles.lower}>
+            <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+            <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>
+            <Text style={styles.location}>{location}</Text>
+            <Text style={styles.aqi}>{aqi}</Text>
+        </View>
+        </LinearGradient>
+    );
+}
+
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    weatherName: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    aqi: PropTypes.number.isRequired
+}
+
+export default Weather;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -34,7 +117,8 @@ const styles = StyleSheet.create({
 
     },
     temp: {
-        fontSize: 48
+        fontSize: 48,
+        color: 'white'
     },
 
     lower: {
@@ -53,5 +137,15 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: "800",
         marginBottom: 160
+    },
+    location: {
+        fontSize: 24,
+        color: 'white',
+        fontWeight: "100",
+    },
+    aqi: {
+        fontSize: 24,
+        color: 'white',
+        fontWeight: "100",
     },
 });
